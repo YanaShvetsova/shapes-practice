18b4fb006f8a1ae6c83ae128a9ac0e1a4e17feb8
Yana Shvetsova <jane66794@gmail.com>
2023-05-03 08:14:35 -0500

    shape

--- src/Circle.java
+++ src/Circle.java
@@ -6,23 +6,29 @@
  */
 
 //implement Shape interface and provide abstract method implementation
-public class Circle{
+public class Circle implements Shape{
 
     /*
     Define an instance variable called as pi and give initial value as 3.14
     Instance variable must be private, static and final
     double pi
      */
-
-
-
+    private static final double pi = 3.14;
 
     /*
     Define an instance variable for Circle called as radius
     Instance variable must be encapsulated
     double radius
      */
+    private double radius;
 
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 
 
     /*
@@ -34,9 +40,23 @@ public class Circle{
     Perimeter of a circle can be found as => 2 * π * r
     Basically it is => 2 * pi * radius
     */
+    @Override
+    public double area() {
+        return pi * radius * radius;
+    }
 
+    @Override
+    public double perimeter() {
+        return 2 * pi * radius;
+    }
 
 
     //Override toString() method here
 
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
 }
